321514ec9e787d11f3882a8ed2221aba3a6f60c7
Yusuf Ibrahim <Yusuf.ibrahim@lineview.com>
2024-12-09 09:38:40 +0000

    Added REST method for forms + bootstrap

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express'); // Import Express
 const app = express(); // Create an Express app
-
+const path = require('path'); // Import Path module for working with file paths
 const PORT = 3000; // Define a port number
 
-// Set up a basic route
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Default route to serve index.html
 app.get('/', (req, res) => {
-    res.send('Welcome to the Riddle App!');
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/add-riddle', (req, res) => {
+    res.sendFile(path.join(__dirname, 'features/add-riddle','add-riddle.html'));
+});
+
+app.get('/riddle-list', (req, res) => {
+    res.sendFile(path.join(__dirname, 'features/riddle-list','riddle-list.html'));
 });
 
 // Start the server
